ec1cd75e8a5e5f65cd2eb06049d2dcfa1446ae20
arieri <ariericyron@gmail.com>
2022-09-09 07:47:11 +0300

    added routes

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import TopBar from "./components/topbar/TopBar";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  const user = false
+  return (
+<Router>
+        <TopBar />
+        <Routes>
+          <Route path="/" exact element={<Home></Home>}></Route> 
+          <Route path="/register" element={user ? <Home/> : <Register />} ></Route>
+          <Route path="/login" element={user ? <Home/> : <Login />} ></Route>
+          <Route path="/write" element={user ? <Write /> : <Register />} ></Route>
+          <Route path="/settings" element={user ? <Settings /> : <Register/>} ></Route>
+          <Route path="/post/:postId" element={<Single />} ></Route> 
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
